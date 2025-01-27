eac633aaeca0c070c9056ab481a2e79b98c3b8f0
Eric Gerberick <188939751+gerby876@users.noreply.github.com>
2025-01-27 16:24:31 -0500

    Have new grid button replace old grid

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
 const container = document.querySelector(".container");
-const row = document.querySelector("row")
 
 const button = document.createElement("button")
 button.textContent = "New Grid"
 button.addEventListener("click", () => {
+    const childCount = container.childElementCount
+    console.log(childCount)
+    if (childCount>1) {
+        removeRows(childCount)
+    }
    let numBoxes =  prompt("Enter grid size", 4);
-   creategrid(numBoxes)
+   if (numBoxes>64 || numBoxes < 1) {
+    alert("Try between 1 and 64.")
+} else {
+   createGrid(numBoxes)}
 })
 container.appendChild(button)
 
-function creategrid(numBoxes) {
+function removeRows(childCount) {
+    const rows = document.getElementsByClassName("row");
+    console.log(rows)
+    for (y=0; y<rows.length; y) {
+        container.removeChild(container.lastChild);
+    }
+}
+
+function createGrid(numBoxes) {
     for (x=0; x<numBoxes; x++) {
         const row = document.createElement("div");
         row.classList.add("row")
