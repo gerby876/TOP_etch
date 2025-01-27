c96c5510174b980cd4ef78aa91ddb31063163393
Eric Gerberick <188939751+gerby876@users.noreply.github.com>
2025-01-27 13:55:56 -0500

    Add creategrid function to start grid get rows to work with creategrid

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
 const container = document.querySelector(".container");
 
 function creategrid(numBoxes) {
-    
-}
-
-function createrow (numBoxes) {
     for (i=0; i<numBoxes; i++) {
         const row = document.createElement("div");
-        row.classList.add("row");
-        container.appendChild(row);
+        row.classList.add("row")
+        container.appendChild(row)
+        function createbox (numBoxes) {
+            for (i=0; i<numBoxes; i++) {
+                const box = document.createElement("div");
+                box.classList.add("box");
+                row.appendChild(box);
+            }
+        }
+        createbox(numBoxes)
     }
 }
 
-function createbox (numBoxes) {
-    for (i=0; i<numBoxes; i++) {
-        const box = document.createElement("div");
-        box.classList.add("box");
-       row.appendChild(box);
-    }
-}
-
-
-createrow(8)
+creategrid(7)
